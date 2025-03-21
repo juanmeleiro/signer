03993470171912ed32f5fa90aa3a8be731dcbedf
Juan F. Meleiro <juan@juanmeleiro.mat.br>
2025-03-21 12:21:39 -0300

    Implement token creation logic

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-import express from 'express';
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const fs = require("node:fs");
 
 const app = express();
 const port = process.env.PORT ? process.environment.PORT : 8080;
+const secret = fs.readFileSync("private.pem");
+// TODO: Validate `secret` is an ES512 key
 
 app.use(express.json())
 
 app.post('/', (req, res) => {
-	res.send(req.body)
+	if (!req.is("application/json")) { res.status(415).end(); return }
+	if (!req.accepts('json')) { res.status(406).end(); return }
+	try {
+		let token = jwt.sign(
+			req.body,
+			secret,
+			{
+				algorithm: "ES512",
+				expiresIn: "1h"
+			}
+		)
+		res.send({
+			auth_token: token
+		})
+	} catch(e) {
+		console.log(e)
+		res.status(500).end()
+		return
+	}
 })
 
 app.listen(port, () => {
