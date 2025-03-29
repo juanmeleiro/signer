4f488932c39ff2536a0f7a28cf574256444f19d4
Juan F. Meleiro <juan@juanmeleiro.mat.br>
2025-03-29 15:03:36 -0300

    Address errors from testing with Cats

--- main.js
+++ main.js
@@ -8,10 +8,24 @@ const secret = fs.readFileSync("private.pem");
 // TODO: Validate `secret` is an ES512 key
 
 app.use(express.json())
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+        return res.status(400).end()
+    }
+    next();
+});
+app.use((req, res, next) => {
+	res.set('Content-Security-Policy', "script-src 'self'")
+	res.set('Cache-Control', 'no-store')
+	res.set('X-Content-Type-Options', 'nosniff')
+    next();
+});
 
 app.post('/', (req, res) => {
 	if (!req.is("application/json")) { res.status(415).end(); return }
 	if (!req.accepts('json')) { res.status(406).end(); return }
+	if (req.body.constructor != Object) { console.log("Request with body that is not an object."); res.status(400).end(); return }
+	console.log("Signing: ", req.body, " of type '", typeof(req.body), "'")
 	try {
 		let token = jwt.sign(
 			req.body,
--- test.sh
+++ test.sh
@@ -14,7 +14,6 @@ cleanup() {
 		case "$1" in
 			docker)
 				docker stop signer
-				docker rm signer
 			;;
 			keys)
 				rm -r test.private.pem test.public.pem
@@ -23,6 +22,9 @@ cleanup() {
 		shift
 	done
 }
+
+# 0. Verify schema
+check-jsonschema --schemafile openapi.json api.json
 
 # 1. Prepare environment
 printf '\033[1mPREPARATION\033[0m\n'
@@ -41,15 +43,21 @@ openssl ec -in test.private.pem -pubout -out test.public.pem || {
 }
 
 # 2. Run the image in a container with some open port..
-docker run -d --name signer -v "$PWD/test.private.pem:/app/private.pem:ro" signer || {
+printf '\033[1mRunning container...\033[0m\n'
+docker rm signer
+docker run -d --name signer -p 8080:8080 -v "$PWD/test.private.pem:/app/private.pem:ro" signer || {
 	cleanup docker keys
 	quit 1 "Couldn't start container."
 }
 
+sleep 10
+
 # ----------------------------------------
 # Do tests
 # 3. Generate a random JSON object according to some reasonable distribution.
+printf '\033[1mRunning tests...\033[0m\n'
 
+./cats -c api.json -s http://localhost:8080
 # 4. Perform the tests as described in [the specification](spec.md) against the open port.
 
 # 5. Repeat from 2 for a sufficiently large number of times.
